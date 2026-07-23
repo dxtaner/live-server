fd1cedc16a403a08b2d3bae847c8bdd30a6710fa
Taner Özer <tanerozer16@gmail.com>
2026-07-23 13:01:11 +0300

    User js added

---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avatar: { type: String, default: "" },
+    streamKey: { type: String, unique: true },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("User", userSchema);
